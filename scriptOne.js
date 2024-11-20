7774fb3f347d8628cf8865d6e61e1c8d048a4d4d
abc54njit <abc54@njit.edu>
2024-11-20 11:55:20 -0500

    hjh

---
+++ scriptOne.js
@@ -0,0 +1,85 @@
+// Q1
+let name = "Aaliyah Crate";
+let age = 18; 
+
+console.log("Name:", name);
+console.log("Age:", age); 
+
+//Q2
+let length = 78;
+let weight = 8;
+
+
+let color = "red";
+let lastName = "ruby";
+
+
+let x = true;
+let y = false;
+
+let undefinedVariable;
+
+console.log("Value of undefined variable:", undefinedVariable); 
+
+  //Q3
+
+  let rollNumber = 15;
+
+if (rollNumber > 10) {
+  console.log("The rollNumber is greater than 10");
+}
+
+//Q4
+
+let isMember = true;
+
+
+if (isMember) {
+    console.log("Member discount applied.");
+}
+
+//Q5
+
+let startValue = 50;
+
+if (startValue > 0) {
+    console.log("The value is positive.");
+} else if (startValue < 0) {
+    console.log("The value is negative.");
+} else {
+    console.log("The value is zero.");
+}
+
+//Q6
+
+let a = 20;
+let b = 10;
+
+console.log("a == b:", a == b);   
+console.log("a === b:", a === b); 
+console.log("a != b:", a != b);   
+console.log("a > b:", a > b);     
+console.log("a < b:", a < b);     
+console.log("a >= b:", a >= b);   
+console.log("a <= b:", a <= b);  
+
+a == b; false ;
+a === b; false ;
+a != b; true ;
+a > b; true ;
+a < b; false ; 
+a >= b; true ;
+a <= b; false ;
+
+
+//Q7
+
+
+let firstname = "Aaliyah";
+let lastname = "Crate";
+let userage = 18;
+
+console.log("Hello, " + Aaliyah + " " + Crate + ", you are " + 18 + " years old.");
+
+
+
